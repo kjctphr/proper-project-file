83151edc9233567fe5fce9610bee03eba73933fb
kjctphr <77785930+kjctphr@users.noreply.github.com>
2021-03-18 22:01:02 +0800

    18Mar 10pm

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
 import os
 import glob
+from mymodules import *
 course = "FHM1234"
+course_list = []
+course_list_ = []
 files = glob.glob(os.path.join("attendance folder",course,"*.txt"))
+print(len(files))
 for file in files:
-    with open(file,"r")as f:
-        print(f.readlines()[0])
+    print(file)
+    callTXTintoNestedList(file,course_list)
+course_list.sort()
+print(course_list)
+for x in range(0,len(course_list),2):
+    course_list_.append([0])
+
+
+# print(course_list_)
